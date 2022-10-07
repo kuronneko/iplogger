8190ddd4451903be06f160eeec23da4000ded249
wdocker <wdocker@wdocker.com>
2022-10-06 22:54:43 -0300

    dashboard view and search bar implementeded

--- app/Http/Controllers/LoggerController.php
+++ app/Http/Controllers/LoggerController.php
@@ -15,8 +15,25 @@ class LoggerController extends Controller
      */
     public function index()
     {
-        $loggers = Logger::all();
-        return Inertia::render('Dashboard', compact('loggers'));
+
+        $loggers = Logger::orderBy("id");
+
+        //request()->session()->flash('message', 'Mensaje de prueba');
+
+        $search = "";
+        if (request()->has("search")) {
+            $search = request("search");
+            $loggers = $loggers->where('ip', 'like', '%' . $search . "%")
+                ->orWhere('host', 'like', '%' . $search . "%");
+        }
+
+        $loggers = $loggers->paginate(10)->appends(request()->except("page"));
+
+        //dd($users);
+        return Inertia::render('Dashboard', compact('loggers','search'));
+
+        //$loggers = Logger::paginate(5);
+        //return Inertia::render('Dashboard', compact('loggers'));
     }
 
     /**
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,60 +8,51 @@
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                            <form method="get" class="flex mt-2" @submit.prevent="submit">
+                                <jet-input v-model="form.search" class="w-full"
+                                    placeholder="Search by IP or Host" type="text" />
+                                <button type="submit" class="ml-2 btn btn-primary">Search</button>
+                            </form>
+                            <table class="table-auto w-full">
+                                <thead>
+                                    <tr>
+                                        <th class="p-3">IP</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="logger in loggers.data" :key="logger.id">
+                                        <td class="p-3 border">{{ logger.ip }}</td>
+                                        <td class="p-3 border">
+                                            <Link class="btn btn-primary mr-2 inline-block" :href="
+                                                route('logger.show', {
+                                                    logger: logger,
+                                                })
+                                            ">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                                viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                            </svg>
+                                            </Link>
+                                            <Link method="DELETE" :href="
+                                                route('logger.destroy', {
+                                                    logger: logger,
+                                                })
+                                            ">Borrar</Link>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
 
-                        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                                <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                                    <table class="table-auto w-full">
-                                        <thead>
-                                            <tr>
-                                                <th class="p-3">IP</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr v-for="logger in loggers" :key="logger.id">
-                                                <td class="p-3 border">{{ logger.ip }}</td>
-                                                <td class="p-3 border">
-                                                    <Link class="btn btn-primary mr-2 inline-block" :href="
-                                                        route('logger.show', {
-                                                            logger: logger,
-                                                        })
-                                                    ">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-                                                        viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            stroke-width="2"
-                                                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                                    </svg>
-                                                    </Link>
-                                                    <Link class="btn btn-primary mr-2 inline-block" :href="
-                                                        route('logger.edit', {
-                                                            logger: logger,
-                                                        })
-                                                    ">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-                                                        viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            stroke-width="2"
-                                                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                                    </svg>
-                                                    </Link>
-                                                    <Link method="DELETE" :href="
-                                                        route('logger.destroy', {
-                                                            logger: logger,
-                                                        })
-                                                    ">Borrar</Link>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-
-                                    <paginator class="mt-3" :paginator="loggers" />
-                                </div>
-                            </div>
+                            <paginator class="mt-3" :paginator="loggers" />
                         </div>
+                    </div>
+                </div>
 
 
 
@@ -84,11 +75,12 @@ import Paginator from "@/Components/Paginator.vue";
 export default {
     props: {
         loggers: Object,
+        search: String,
     },
     data() {
         return {
-            logger: {
-                ip: this.ip,
+            form: {
+                search: this.search,
             },
         };
     },
@@ -101,10 +93,8 @@ export default {
         Link,
     },
     methods: {
-        deleteUser: function () {
-            Inertia.delete(
-                route("logger.destroy", { logger: this.logger })
-            );
+        submit: function () {
+            Inertia.get(route("logger.index", this.form));
         },
     },
 };
