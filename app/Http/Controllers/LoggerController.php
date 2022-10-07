cdd29ee2a22dd0c538c82c7979f7f6bfe92be21a
wdocker <wdocker@wdocker.com>
2022-10-07 01:17:48 -0300

    delete log

--- app/Http/Controllers/LoggerController.php
+++ app/Http/Controllers/LoggerController.php
@@ -97,8 +97,9 @@ class LoggerController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Logger $logger)
     {
-        //
+        $logger->delete();
+        return redirect()->back()->with('message', 'Logger deleted');
     }
 }
