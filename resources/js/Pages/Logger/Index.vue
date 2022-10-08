22223bde55d92af609930d00f7e2db40f866e148
wdocker <wdocker@wdocker.com>
2022-10-07 21:45:07 -0300

    $page.props.search

--- resources/js/Pages/Logger/Index.vue
+++ resources/js/Pages/Logger/Index.vue
@@ -17,7 +17,7 @@
                                 placeholder="Search by IP or HOST" type="text" />
                             <jet-primary-button type="submit" class="ml-1">Search
                             </jet-primary-button>
-                            <jet-primary-button v-if="form.search" @click="clear()" class="ml-1">
+                            <jet-primary-button v-if="$page.props.search" @click="clear()" class="ml-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                     stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round"
