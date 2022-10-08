9f8ce28fd097bc7fa0ec205d55e4bab90f7f374f
wdocker <wdocker@wdocker.com>
2022-10-07 21:39:27 -0300

    style and search bar functionality

--- resources/js/Pages/Logger/Index.vue
+++ resources/js/Pages/Logger/Index.vue
@@ -8,51 +8,59 @@
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
 
-                            <form method="get" class="flex mt-2" @submit.prevent="searchLogger">
-                                <jet-input v-model="form.search" id="search" class="w-full" placeholder="Search by IP or HOST"
-                                    type="text" />
-                                <jet-primary-button type="submit" class="ml-2 btn btn-primary">Search
-                                </jet-primary-button>
-                            </form>
+                        <form method="get" class="flex mt-2" @submit.prevent="searchLogger">
+                            <jet-input v-model="form.search" id="search" class="w-full"
+                                @keydown.delete="clearKeyDown()"
+                                placeholder="Search by IP or HOST" type="text" />
+                            <jet-primary-button type="submit" class="ml-1">Search
+                            </jet-primary-button>
+                            <jet-primary-button v-if="form.search" @click="clear()" class="ml-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+                                </svg>
+                            </jet-primary-button>
+                        </form>
 
-                            <div class="py-3">
-                                <div class='overflow-x-auto w-full'>
-                                    <table
-                                        class='mx-auto max-w-7x1 w-full whitespace-nowrap rounded-lg bg-white divide-y divide-gray-300 overflow-hidden'>
-                                        <thead class="bg-gray-800">
-                                            <tr class="text-white text-left">
-                                                <th class="font-semibold text-sm uppercase px-6 py-4"> Date </th>
-                                                <th class="font-semibold text-sm uppercase px-6 py-4"> IP </th>
-                                                <th class="font-semibold text-sm uppercase px-6 py-4">
-                                                    Location </th>
-                                                <th class="font-semibold text-sm uppercase px-6 py-4">
-                                                    Browser </th>
-                                                <th class="font-semibold text-sm uppercase px-6 py-4"> Options</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody class="divide-y divide-gray-200">
-                                            <tr v-for="logger in loggers.data" :key="logger.id">
-                                                <td class="px-6 py-4">
-                                                    <div class="flex items-center space-x-3">
-                                                        <div>
-                                                            <p> {{ logger.updated_at }} </p>
-                                                        </div>
+                        <div class="py-3">
+                            <div class='overflow-x-auto w-full'>
+                                <table
+                                    class='mx-auto max-w-7x1 w-full whitespace-nowrap rounded-lg bg-white divide-y divide-gray-300 overflow-hidden'>
+                                    <thead class="bg-gray-800">
+                                        <tr class="text-white text-left">
+                                            <th class="font-semibold text-sm uppercase px-6 py-4"> Date </th>
+                                            <th class="font-semibold text-sm uppercase px-6 py-4"> IP </th>
+                                            <th class="font-semibold text-sm uppercase px-6 py-4">
+                                                Location </th>
+                                            <th class="font-semibold text-sm uppercase px-6 py-4">
+                                                Browser </th>
+                                            <th class="font-semibold text-sm uppercase px-6 py-4"> Options</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="divide-y divide-gray-200">
+                                        <tr v-for="logger in loggers.data" :key="logger.id">
+                                            <td class="px-6 py-4">
+                                                <div class="flex items-center space-x-3">
+                                                    <div>
+                                                        <p> {{ logger.updated_at }} </p>
                                                     </div>
-                                                </td>
-                                                <td class="px-6 py-4">
-                                                    <p class=""> {{ logger.ip }} </p>
-                                                </td>
-                                                <td class="px-6 py-4">
-                                                    <p class=""> {{ logger.country +','+ logger.city }} </p>
-                                                </td>
-                                                <td class="px-6 py-4">
-                                                    <p class=""> {{ logger.browser }} </p>
-                                                </td>
-                                                <td class="px-6 py-4">
-                                                    <!--                                             <jet-primary-button>
+                                                </div>
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                <p class=""> {{ logger.ip }} </p>
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                <p class=""> {{ logger.country +','+ logger.city }} </p>
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                <p class=""> {{ logger.browser }} </p>
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                <!--                                             <jet-primary-button>
                                                 <Link :href="
                                                     route('logger.show', {
                                                         logger: logger,
@@ -68,7 +76,7 @@
                                                 </svg>
                                                 </Link>
                                             </jet-primary-button> -->
-                                            <!--                                             <Link method="DELETE" :href="
+                                                <!--                                             <Link method="DELETE" :href="
                                                 route('logger.destroy', {
                                                     logger: logger,
                                                 })
@@ -78,27 +86,31 @@
                                                     d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                             </svg>
                                             </Link> -->
-                                            <jet-primary-button @click="showLogger(logger)">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-                                                </svg>
-                                            </jet-primary-button>
-                                            <jet-primary-button @click="deleteLogger(logger)">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                                </svg>
-                                            </jet-primary-button>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
+                                                <jet-primary-button @click="showLogger(logger)" class="ml-1">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                        class="w-6 h-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                                                    </svg>
+                                                </jet-primary-button>
+                                                <jet-danger-button @click="deleteLogger(logger)" class="ml-1">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                        class="w-6 h-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                                    </svg>
+                                                </jet-danger-button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
-                            <paginator class="mt-3" :paginator="loggers" />
                         </div>
+                        <paginator class="mt-3" :paginator="loggers" />
                     </div>
+                </div>
             </div>
         </div>
     </AppLayout>
@@ -111,6 +123,7 @@ import AppLayout from "@/Layouts/AppLayout.vue";
 
 //import JetDialogModal from "@/Components/DialogModal";
 import JetPrimaryButton from "@/Components/PrimaryButton.vue";
+import JetDangerButton from "@/Components/DangerButton.vue";
 import JetInput from "@/Components/Input.vue";
 import { Link } from "@inertiajs/inertia-vue3";
 import Paginator from "@/Components/Paginator.vue";
@@ -132,6 +145,7 @@ export default {
         AppLayout,
         //JetDialogModal,
         JetPrimaryButton,
+        JetDangerButton,
         //JetButton,
         JetInput,
         Paginator,
@@ -141,10 +155,10 @@ export default {
         showLogger: function (logger) {
             this.$swal({
                 position: 'center',
-                color: '#fff',
-                confirmButtonColor: '#198754',
+                color: '#00000',
+                confirmButtonColor: '#212529',
                 width: 600,
-                background: '#212529',
+                background: '#fff',
                 text: '' + logger.ip + ',' + logger.host + ',' + logger.browser + '',
                 title: 'Logger Detail',
                 html: `
@@ -161,22 +175,22 @@ export default {
                 title: 'Are you sure?',
                 text: "You won't be able to revert this!",
                 icon: 'warning',
-                background: '#212529',
-                color: '#fff',
+                background: '#fff',
+                color: '#00000',
                 width: 400,
                 position: 'center',
                 showCancelButton: true,
-                confirmButtonColor: '#198754',
-                cancelButtonColor: '#d33',
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#212529',
                 confirmButtonText: 'Yes, delete it!'
             }).then((result) => {
                 if (result.isConfirmed) {
                     Inertia.delete(route("logger.destroy", logger));
                     this.$swal({
                         position: 'center',
-                        color: '#fff',
+                        color: '#00000',
                         width: 400,
-                        background: '#212529',
+                        background: '#fff',
                         icon: 'success',
                         title: 'Logger deleted successfully',
                         showConfirmButton: false,
@@ -187,12 +201,18 @@ export default {
             })
         },
         searchLogger: function () {
-            Inertia.get(route("logger.index", this.form));
+                if(this.form.search !== ''){
+                    Inertia.get(route("logger.index", this.form));
+                }
+        },
+        clearKeyDown: function () {
+            if(this.form.search == ''){
+                Inertia.get(route("logger.index"));
+            }
         },
         clear: function () {
-            if (this.form.search == '') {
-                this.searchLogger();
-            }
+            this.form.search = '';
+            Inertia.get(route("logger.index"));
         }
     },
 };
