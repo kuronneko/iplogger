76a61e92548296aaf7fd1217c8200d4f52506a1a
wdocker <wdocker@wdocker.com>
2022-10-11 13:51:43 -0300

    replace indigo color to neutral, and new logo

--- resources/js/Components/Checkbox.vue
+++ resources/js/Components/Checkbox.vue
@@ -31,6 +31,6 @@ const proxyChecked = computed({
         v-model="proxyChecked"
         type="checkbox"
         :value="value"
-        class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        class="rounded border-gray-300 text-neutral-600 shadow-sm focus:border-neutral-300 focus:ring focus:ring-neutral-200 focus:ring-opacity-50"
     >
 </template>
