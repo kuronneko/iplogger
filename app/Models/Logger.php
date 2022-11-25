8a0f4c9229dad567d6ebea268fdc49cecd2bd1e9
wdocker <wdocker@wdocker.com>
2022-11-25 04:33:27 -0300

    fillable new fields added

--- app/Models/Logger.php
+++ app/Models/Logger.php
@@ -11,7 +11,7 @@ class Logger extends Model
 
     use HasFactory;
 
-    protected $fillable = ['user_id','ip','country','city','browser','browser_version','platform','platform_version','agent','host'];
+    protected $fillable = ['user_id','ip','country','city','browser','browser_version','platform','platform_version','agent','host', 'created_at', 'updated_at'];
 
     public function user(){
         return $this->belongsTo(User::class, 'user_id');
