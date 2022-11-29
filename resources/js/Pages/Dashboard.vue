0e5f1a4feed62479e9eb5b07ca0c410c0e411253
wdocker <wdocker@wdocker.com>
2022-11-28 21:56:46 -0300

    save animation button and padding

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -24,8 +24,10 @@
                             <div class="flex">
                                 <TextInput readonly v-model="url" type="text" class="mt-1 block w-full"
                                     autocomplete="url" />
-                                <PrimaryButton @click="copyUrl()" class="ml-1">Copy
-                                </PrimaryButton>
+                                <div>
+                                    <PrimaryButton @click="copyUrl()" class="ml-1 py-3 mt-1">Copy
+                                    </PrimaryButton>
+                                </div>
                             </div>
                         </div>
 
@@ -34,10 +36,14 @@
                             <div class="flex">
                                 <TextInput readonly v-model="shortUrl" type="text" class="mt-1 block w-full"
                                     autocomplete="url" />
-                                <PrimaryButton @click="generateShortUrl(user)" v-if="user.setting.slug === null" class="ml-1">Generate
-                                </PrimaryButton>
-                                <PrimaryButton @click="copyShortUrl()" v-if="user.setting.slug" class="ml-1">Copy
-                                </PrimaryButton>
+                                <div>
+                                    <PrimaryButton @click="generateShortUrl(user)" v-if="user.setting.slug === null"
+                                        class="ml-1 py-3 mt-1">Generate
+                                    </PrimaryButton>
+                                    <PrimaryButton @click="copyShortUrl()" v-if="user.setting.slug"
+                                        class="ml-1 py-3 mt-1">Copy
+                                    </PrimaryButton>
+                                </div>
                             </div>
                         </div>
 
@@ -76,10 +82,25 @@
                                 <TextInput v-model="redirect" :disabled="user.setting.silence_mode == 1" id="redirect"
                                     type="text" class="mt-1 block w-full"
                                     :class="v$.redirect.$error === true ? 'border-gray-300 focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm' : ''" />
-                                <PrimaryButton
-                                    :disabled="user.setting.silence_mode == 1 || this.isLoading === true" class="ml-1">
-                                    Save
-                                </PrimaryButton>
+                                <div>
+                                    <PrimaryButton v-if="this.isLoading === false"
+                                        :disabled="user.setting.silence_mode == 1" class="ml-1 py-3 mt-1">
+                                        Save
+                                    </PrimaryButton>
+                                    <PrimaryButton v-if="this.isLoading === true" :disabled="this.isLoading === true"
+                                        class="ml-1 py-3 mt-1">
+                                        <svg class="inline mr-2 w-3 h-3 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
+                                            viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                fill="currentColor" />
+                                            <path
+                                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                fill="currentFill" />
+                                        </svg>
+                                        Saving
+                                    </PrimaryButton>
+                                </div>
                             </form>
                             <span class="text-danger small" v-for="error of v$.redirect.$errors" :key="error.$uid">
                                 {{ error.$message }}
@@ -88,14 +109,14 @@
                     </template>
                 </FormSection>
 
-                    <SectionBorder />
-                    <ExportAllLog class="mt-10 sm:mt-0"/>
+                <SectionBorder />
+                <ExportAllLog class="mt-10 sm:mt-0" />
 
-                    <SectionBorder />
-                    <ImportAllLog class="mt-10 sm:mt-0"/>
+                <SectionBorder />
+                <ImportAllLog class="mt-10 sm:mt-0" />
 
-                    <SectionBorder />
-                    <DeleteAllLog class="mt-10 sm:mt-0"/>
+                <SectionBorder />
+                <DeleteAllLog class="mt-10 sm:mt-0" />
 
             </div>
         </div>
@@ -135,7 +156,7 @@ export default {
             url: this.setUrl(),
             shortUrl: this.setShortUrl(),
             redirect: this.user.setting.redirect,
-            isLoading: null,
+            isLoading: false,
         };
     },
     validations() {
@@ -144,22 +165,22 @@ export default {
         }
     },
     components: {
-    DeleteAllLog,
-    SectionBorder,
-    useVuelidate,
-    FormSection,
-    InputError,
-    InputLabel,
-    TextInput,
-    AppLayout,
-    //JetDialogModal,
-    PrimaryButton,
-    //JetButton,
-    Paginator,
-    Link,
-    ExportAllLog,
-    ImportAllLog,
-},
+        DeleteAllLog,
+        SectionBorder,
+        useVuelidate,
+        FormSection,
+        InputError,
+        InputLabel,
+        TextInput,
+        AppLayout,
+        //JetDialogModal,
+        PrimaryButton,
+        //JetButton,
+        Paginator,
+        Link,
+        ExportAllLog,
+        ImportAllLog,
+    },
     methods: {
         generateShortUrl: function (user) {
             if (user.setting.slug === null) {
@@ -211,19 +232,19 @@ export default {
                         icon: 'success',
                         title: 'Redirect URL saved'
                     })
-                    this.isLoading = null;
+                    this.isLoading = false;
                 }, 1000)
 
             }
         },
         setShortUrl() {
-            if(this.user.setting.slug){
-            //let [firstDetails] = this.personas;// es6 syntax of destructing the array
-            let slug = this.user.setting.slug;
-            let hostname = location.hostname;
-            let protocol = 'https';
-            return protocol + '://' + hostname + '/' + slug;
-            }else{
+            if (this.user.setting.slug) {
+                //let [firstDetails] = this.personas;// es6 syntax of destructing the array
+                let slug = this.user.setting.slug;
+                let hostname = location.hostname;
+                let protocol = 'https';
+                return protocol + '://' + hostname + '/' + slug;
+            } else {
                 return '';
             }
         },
